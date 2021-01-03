ff8ee3f19a7e4b0401ccb3390d0e6e929ef35410
hiroyha <crystalline407@gmail.com>
2021-01-03 17:56:09 +0900

    run an infinite loop; add deployment manifest

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+: :${SLEEP_SECONDS:=60}
 VERSION="2019-08-01"
 URL="http://169.254.169.254/metadata/scheduledevents?api-version=${VERSION}"
 
-echo $(curl -s -H Metadata:true ${URL})
+while :; do
+    echo $(curl -s -H Metadata:true ${URL})
+    sleep ${SLEEP_SECONDS}
+done
